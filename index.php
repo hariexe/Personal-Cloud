838cbe4a0755a7de9cb0094d8db541beec5167a5
hariexe <muhammadnurbahari@gmail.com>
2024-06-02 05:12:46 +0700

    Title change test

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ $days_remaining = ceil(($trial_end_date - $current_date) / 86400);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Files - BiJi Cloud</title>
+    <title>My Files ss - BiJi Cloud</title>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
